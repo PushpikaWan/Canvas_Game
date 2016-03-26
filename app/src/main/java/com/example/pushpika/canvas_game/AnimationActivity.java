7c30b2301e80d4b2803cfbfb70ab6f2bd3b02aa2
PushpikaWan <lucefer41@gmail.com>
2016-03-26 13:53:50 +0530

    Animation are initialized

--- app/src/main/java/com/example/pushpika/canvas_game/AnimationActivity.java
+++ app/src/main/java/com/example/pushpika/canvas_game/AnimationActivity.java
@@ -9,23 +9,20 @@ import android.view.View;
 
 
 public class AnimationActivity extends AppCompatActivity {
-    Activity_animation_layout activity_animation_layout;
-    public static int position_width=0,position_height=0,cur_position=1,is_increment=1;
+    //Activity_animation_layout activity_animation_layout;
+    board_layout bd_layout;
+    public static int position_width=0,position_height=0,cur_position=1,is_increment=0,current_pos=13,target_pos=7;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //getActionBar().hide();
-        activity_animation_layout = new Activity_animation_layout(this);
+        //activity_animation_layout = new Activity_animation_layout(this);
+        bd_layout = new board_layout(this);
         getSupportActionBar().hide();
-        setContentView(activity_animation_layout);
-
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-
-
+        //setContentView(activity_animation_layout);
+        setContentView(bd_layout);
 
 
     }
---
+++ app/src/main/java/com/example/pushpika/canvas_game/board_layout.java
@@ -0,0 +1,138 @@
+package com.example.pushpika.canvas_game;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by pushpika on 3/26/16.
+ */
+public class board_layout extends View {
+
+
+    int target_height,target_width;
+    Paint red_paint_brush_full,green_paint_brush_full,blue_paint_brush_full;
+    Bitmap black_circle;
+    int move=0;
+    int black_circlex,black_circley,x_dir,y_dir;
+    int move_forward,move_backword,move_upward,move_downward;
+    String [] transition_sequence={"N","N","N","N","N","N","N","N","N","N","N","N","N","N","N","N","N","N","N","N"}; //20 none,F -forward,B -backward ,U -upward, D -downward
+    int current_position,target_position;
+    public board_layout(Context context) {
+        super(context);
+        setBackgroundResource(R.drawable.board);
+        x_dir = 50;
+        y_dir = 50;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        red_paint_brush_full = new Paint();
+        red_paint_brush_full.setColor(Color.RED);
+        red_paint_brush_full.setStyle(Paint.Style.FILL);
+
+        green_paint_brush_full = new Paint();
+        green_paint_brush_full.setColor(Color.GREEN);
+        green_paint_brush_full.setStyle(Paint.Style.FILL);
+
+        blue_paint_brush_full = new Paint();
+        blue_paint_brush_full.setColor(Color.BLUE);
+        blue_paint_brush_full.setStyle(Paint.Style.FILL);
+
+
+        black_circle = BitmapFactory.decodeResource(getResources(), R.drawable.blackcircle);
+
+
+        if (AnimationActivity.position_height==0 && AnimationActivity.position_width==0){ //if variable doesn't initalize
+            AnimationActivity.position_height = (int) Math.ceil((canvas.getHeight()/10*8+(canvas.getHeight()/50)));   //
+            AnimationActivity.position_width = (int) Math.ceil((canvas.getWidth())/10*7+(canvas.getWidth()/100)); //width ok
+        }
+
+        //create transition sequence
+        if(AnimationActivity.is_increment==1) {
+            //when increment
+            Log.v("happened increment",String.valueOf(AnimationActivity.target_pos));
+            int i = 0;
+            while (AnimationActivity.current_pos + 1 <= AnimationActivity.target_pos) {
+                if ((AnimationActivity.current_pos) % 10 != 0) {
+                    if ((int) Math.floor(AnimationActivity.current_pos / 10) % 2 == 0) { //move forward
+                        move_forward += 1;
+                        transition_sequence[i] = "F";
+                        Log.v("happened move",  "1 forward");
+                    } else { //move backward
+                        move_backword += 1;
+                        transition_sequence[i] = "F";
+                        Log.v("happened move",  "1 backward");
+                    }
+                } else { //move upward
+                    move_upward += 1;
+                    transition_sequence[i] = "U";
+                    Log.v("happened move",  "1 upward");
+                }
+                AnimationActivity.current_pos += 1;
+                i++;
+            }
+        }
+        else if(AnimationActivity.is_increment==0) {
+            Log.v("happened decrement",String.valueOf(AnimationActivity.target_pos));
+            //when decrement
+            int j = 0;
+            while (AnimationActivity.current_pos - 1 >= AnimationActivity.target_pos) {
+                if ((AnimationActivity.current_pos - 1) % 10 != 0) {
+                    if ((int) Math.floor(AnimationActivity.current_pos / 10) % 2 != 0) { //move forward
+                        move_forward += 1;
+                        transition_sequence[j] = "F";
+                        Log.v("happened move",  "1 forward");
+                    } else { //move backward
+                        move_backword += 1;
+                        transition_sequence[j] = "B";
+                        Log.v("happened move",  "1 backward");
+                    }
+                } else { //move upward
+                    move_downward += 1;
+                    transition_sequence[j] = "D";
+                    Log.v("happened move",  "1 downward");
+                }
+                AnimationActivity.current_pos -= 1;
+                j++;
+            }
+        }
+
+        //canvas.drawBitmap(black_circle,AnimationActivity.position_width,AnimationActivity.position_height, null);
+
+        //up _code
+        if (transition_sequence[move].equals("U")) {
+            AnimationActivity.position_height = AnimationActivity.position_height - (int) Math.ceil((canvas.getHeight() / 10 * 1));   //
+        }
+
+        // down _code
+        else if (transition_sequence[move].equals("D")) {
+            AnimationActivity.position_height = AnimationActivity.position_height + (int) Math.ceil((canvas.getHeight() / 10 * 1));   //
+        }
+
+        // move_forward _code
+        else if (transition_sequence[move].equals("F")) {
+            AnimationActivity.position_width = AnimationActivity.position_width + (int) Math.ceil((canvas.getWidth()) / 10 * 1);
+        }
+
+        //move_backward _code
+        else if (transition_sequence[move].equals("B")) {
+            AnimationActivity.position_width = AnimationActivity.position_width - (int) Math.ceil((canvas.getWidth()) / 10 * 1);
+        }
+
+        canvas.drawBitmap(black_circle,AnimationActivity.position_width,AnimationActivity.position_height, null);
+
+        move=move+1;
+        AnimationActivity.is_increment=-1; //loop closed
+        if(!transition_sequence[move].equals("N")){
+            invalidate();
+        }
+    }
+
+}
